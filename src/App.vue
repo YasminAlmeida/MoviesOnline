161ec1dc35e302562dbd3f8c660cfe71e0e5a58d
yasmin <yasmin.almeidavd@gmail.com>
2022-08-16 19:47:26 -0400

    feat(Slide Addition): Addition of Vueper Slides library

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ ul {
   list-style: none;
 }
 body {
+  /* background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab); */
   background: #000;
 }
 nav a.router-link-exact-active {
--- src/components/TheMovies.vue
+++ src/components/TheMovies.vue
@@ -2,67 +2,111 @@
   <section>
     <div class="popular">
       <h3 class="title">{{ title }}</h3>
-      <div class="slide">
-        <ul v-for="(results, index) in api.results" :key="index">
-          <li>
-            <!-- <p>{{ results.overview }}</p> -->
-            <img
-              :src="`${this.baseurl}${results.poster_path}`"
-              class="posters"
-            />
-            <div class="info">
-              <p>{{ results.title }}</p>
-              <span>{{ results.vote_average }}</span>
+      <vueper-slides
+        class="no-shadow"
+        :visible-slides="4"
+        slide-multiple
+        :gap="2"
+        :dragging-distance="100"
+        :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }"
+      >
+        <vueper-slide
+          class="slide"
+          v-for="(results, index) in api.results"
+          :key="index"
+        >
+          <template #content>
+            <div>
+              <img
+                :src="`${this.baseurl}${results.poster_path}`"
+                class="posters"
+              />
+              <div class="info">
+                <p>{{ results.title }}</p>
+                <span>{{ results.vote_average }}</span>
+              </div>
             </div>
-          </li>
-        </ul>
-      </div>
+          </template>
+        </vueper-slide>
+      </vueper-slides>
     </div>
   </section>
 </template>
 
 <script>
 import { api } from "@/TheServices.js";
+import { VueperSlides, VueperSlide } from "vueperslides";
+import "vueperslides/dist/vueperslides.css";
+
 export default {
   name: "TheMovies",
   props: ["types", "title"],
+  components: { VueperSlides, VueperSlide },
   data() {
     return {
+      slides: [
+        {
+          content: "",
+        },
+      ],
       api: [],
-      // types: "popular",
       baseurl: "https://image.tmdb.org/t/p/w500",
     };
   },
+
   methods: {
     getMovies() {
       api.get(this.types).then((response) => {
-        (this.api = response.data), console.log(response.data);
+        this.api = response.data;
       });
     },
   },
   created() {
     this.getMovies();
-    console.log(this.title);
   },
 };
 </script>
 <style>
+.vueperslides__arrows--outside .vueperslides__arrow--next,
+.vueperslides--rtl .vueperslides__arrows--outside .vueperslides__arrow--prev {
+  left: auto;
+  right: 0;
+}
+.vueperslides__arrow svg {
+  color: #9b30f9;
+  width: 3em;
+}
+.vueperslides__arrow {
+  opacity: 1;
+  background: #fff;
+  border-radius: 50%;
+}
+.vueperslides__arrows--outside .vueperslides__arrow--prev,
+.vueperslides--rtl .vueperslides__arrows--outside .vueperslides__arrow--next {
+  right: auto;
+  left: 0;
+}
+.vueperslides__bullets,
+.vueperslides__bullets--outside {
+  color: #aa88ff;
+}
+.vueperslides__bullets {
+  position: relative;
+}
 .title {
   color: #fff;
 }
 .slide {
-  overflow-x: scroll;
-  overflow-y: hidden;
   display: flex;
-  /* overflow: hidden; */
-  align-items: flex-start;
-  /* overflow-x: auto; */
-  transition: height 0.5s linear;
+  justify-content: center;
+  align-items: center;
+}
+.vueperslides__parallax-wrapper {
+  padding-bottom: 50% !important;
 }
 .posters {
-  width: 150px;
+  max-width: 300px;
   border-radius: 8px;
-  min-width: 150px;
 }
 .info {
   display: grid;
@@ -77,5 +121,37 @@ export default {
   padding: 2px;
   border-radius: 50%;
   font-size: 0.8rem;
+}
+@media (max-width: 1200px) {
+  .vueperslide,
+  .vueperslide--active,
+  .vueperslide--visible,
+  .slide {
+    width: 50% !important;
+    margin-right: 5% !important;
+  }
+}
+
+@media (max-width: 830px) {
+  .posters {
+    width: 300px;
+  }
+  .vueperslides__parallax-wrapper {
+    padding-bottom: 90% !important;
+  }
+}
+@media (max-width: 760px) {
+  /* .vueperslide,
+  .vueperslide--active,
+  .vueperslide--visible,
+  .slide {
+    width: 50% !important;
+    margin-right: 5% !important;
+    margin-left: 0% !important;
+  } */
+  .posters {
+    width: 250px;
+    /* margin-left: 50px; */
+  }
 }
 </style>
